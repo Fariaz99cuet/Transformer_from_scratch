c2beef2c9e6516ca80a0adde19c2a8ddf2195202
Fariaz Islam <103488785+Fariaz99cuet@users.noreply.github.com>
2024-04-17 15:33:28 +0600

    config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 4,
+        "batch_size": 8,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
         "datasource": 'opus_books',
         "lang_src": "en",
-        "lang_tgt": "it",
+        "lang_tgt": "ru",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
